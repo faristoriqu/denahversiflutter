a33272a60614ff8a674a67e54b742d30ca271a68
faristoriqu <mtoriqalfariziimansyah@gmail.com>
2021-05-08 22:15:16 +0700

    masih sampai dialog

---
+++ lib/denah/main.dart
@@ -0,0 +1,37 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+int i = 0;
+void tahu() {
+  i++;
+  log("data : $i");
+}
+
+Future<void> dialogku(context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('AlertDialog Title'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('This is a demo alert dialog.'),
+              Text('Would you like to approve of this message?'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
---
+++ lib/res/Strings.dart
@@ -0,0 +1,4 @@
+class Strings {
+  static const String welcomeMessage = "Denah";
+  static const String beranda = "Beranda";
+}
